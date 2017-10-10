bcfd549d2bb359bbf59fab7e1e43a8e145e380a8
artcom <artcom@HOLODECK1>
2017-10-10 16:22:54 +0200

    full export of material properties together with fbx-export

--- python/packages/win64/material_collector.py
+++ python/packages/win64/material_collector.py
@@ -55,6 +55,73 @@ MATERIAL_OPTIONAL_CHANNELS = [
     "reflection", "glow", "displacement", "diffusion"
 ]
 
+class Material:
+    id, doc, name, c4d_material, tag_data = (None, None, None, None, None)
+
+    def __init__(self, c4d_material, doc):
+        if not isinstance(c4d_material, c4d.BaseMaterial):
+            raise ValueError("1st parameter is of invalid type: {}".format(type(c4d_material)))
+        self.c4d_material = c4d_material
+        self.name = c4d_material.GetName()
+        self.id = id(c4d_material)
+        self.tag_data = {}
+        self.doc = doc
+        for name, channel in MATERIAL_CHANNELS.iteritems():
+            channel_data, shader_data = self.probe_channel(c4d_material, channel, name)
+            if not channel_data is None:
+                self.tag_data[name] = channel_data
+                if not shader_data is None:
+                    self.tag_data[name]["shader"] = shader_data
+    
+    def probe_channel(self, mat, chan, chan_name):
+        if not chan_name in MATERIAL_NEEDED_CHANNELS:
+            return (None, None)
+        if not chan_name in SHADER_CHANNELS: # ignores specular_color - mby mistake
+            return (None, None)
+        has_state = mat.GetChannelState(chan)
+        if not has_state:
+            return (None, None)
+        channel_data = collect_material_info(mat, chan_name)
+        shader_data = self.collect_shader_data(channel_data["shader"])
+        # serialize vector - might need patching
+        for name, data_point in channel_data.iteritems():
+            if isinstance(data_point, c4d.Vector):
+                channel_data[name] = {'x': data_point[0], 'y': data_point[1], 'z': data_point[2]}
+        return (channel_data, shader_data)
+
+    def collect_shader_data(self, shader):
+        if shader is None:
+            return
+        data = {}
+        for name, constant in SHADER_CONSTANTS.iteritems():
+            value = shader[constant]
+            if isinstance(value, c4d.Vector):
+                value = {'x': value[0], 'y': value[1], 'z': value[2]}
+            data[name] = value
+            
+        tex_path = c4d.GenerateTexturePath(self.doc[c4d.DOCUMENT_PATH], data["filename"], "")
+        if tex_path is None:
+            tex_path = "INVALID PATH"
+            print("ERR:  Texture {} not found in project folder.".format(data["filename"]))
+            del data["filename"]
+        else:
+            data["filename"] = tex_path
+        return data
+
+    def to_json(self):
+        return {"id": self.id, "name": self.name, "tag_data": self.tag_data}
+
+    def __str__(self):
+        return "Material: name: [{}] - id: [{}]".format(self.name, self.id)
+
+    def __eq__(self, other):
+        if isinstance(other, Material):
+            return self.c4d_material.Compare(other.c4d_material)
+        if isinstance(other, c4d.Material):
+            return self.c4d_material.Compare(other)
+        else:
+            return False
+
 
 def get_all_children(obj):
     all_seen = [obj]
@@ -77,70 +144,35 @@ def collect_all_objects(objects):
 
 def collect_data(doc):
     document = c4d.documents.GetActiveDocument()
-    objects = collect_all_objects(document.GetObjects())
-    materials = doc.GetMaterials()
-    
-    data = []
+    objects = collect_all_objects(document.GetObjects())    
+
+    mat_data = []
+    obj_data = []
     for obj in objects:
-        sourceTags = obj.GetTags()
-        for tag in sourceTags:
+        material_links = []
+        source_tags = obj.GetTags()
+        for tag in source_tags:
             if not isinstance(tag, c4d.TextureTag):
                 continue
             mat = tag.GetMaterial()
-            if not mat in data:
-                data.append(mat)
-    return objects, data
+            name = mat.GetName()
+            material_links.append({"name": name, "id": id(mat)})
+            if not mat in mat_data:
+                mat_data.append(mat)
+                
+    return objects, mat_data
 
-def collect_shader_data(doc, shader):
-    if shader is None:
-        return
-    data = {}
-    for name, constant in SHADER_CONSTANTS.iteritems():
-        data[name] = shader[constant]
-        
-    tex_path = c4d.GenerateTexturePath(doc[c4d.DOCUMENT_PATH], data["filename"], "")
-    if tex_path is None:
-        tex_path = "INVALID PATH"
-        print("ERR:  Texture {} not found in project folder.".format(data["filename"]))
-    data["filename"] = tex_path
-    return data
-
-def probe_channel(doc, mat, chan, chan_name):
-    if not chan_name in MATERIAL_NEEDED_CHANNELS:
-        return (None, None)
-    if not chan_name in SHADER_CHANNELS: # ignores specular_color - mby mistake
-        return (None, None)
-    has_state = mat.GetChannelState(chan)
-    if not has_state:
-        return (None, None)
-    channel_data = collect_material_info(mat, chan_name)
-    shader_data = collect_shader_data(doc, channel_data["shader"])
-    # serialize vector - might need patching
-    for name, data_point in channel_data.iteritems():
-        if isinstance(data_point, c4d.Vector):
-            channel_data[name] = {'x': data_point[0], 'y': data_point[1], 'z': data_point[2]}
-    return (channel_data, shader_data)
 
 def serialize_materials(doc, materials):
     data = []
     unique_materials = []
     for material in materials:
-        tag_data = {}
-        if material in unique_materials:
+        mat = Material(material, doc)
+        if mat in data:
             continue
         unique_materials.append(material)
-        for name, channel in MATERIAL_CHANNELS.iteritems():
-            channel_data, shader_data = probe_channel(doc, material, channel, name)
-            if not channel_data is None:
-                tag_data[name] = channel_data
-                if not shader_data is None:
-                    tag_data[name]["shader"] = shader_data
-        data.append({
-            "id": id(material),
-            "tag_data": tag_data,
-            "name": material.GetName()
-        })
-    if len(unique_materials) < len(materials):
+        data.append(mat)
+    if len(data) < len(materials):
         print("WARN: Materials showed up twice and have been truncated.")
-    return data
-
+    # throw it out into the void
+    return [mat.to_json() for mat in data]
--- scripts/dataexport.py
+++ scripts/dataexport.py
@@ -1,8 +1,12 @@
 import c4d
 from shutil import copyfile 
-from material_collector import collect_data, serialize_materials
+from material_collector import collect_data, serialize_materials, Material
+from material_strategies import collect_material_info
 import json
 from os import path, makedirs
+from pprint import pprint
+
+EXPORT_FBX = 1026370 # magic number - not documented
 
 def spawn_save_dialog():
     print(c4d.FILESELECTTYPE_ANYTHING)
@@ -23,7 +27,7 @@ def copy_texture_files(serialized_materials, to_path):
         tag_data = mat["tag_data"]
         for key, value in tag_data.iteritems():
             shader = value["shader"]
-            if shader is None:
+            if shader is None or not "filename" in shader:
                 continue
             filename = shader["filename"]
             real_filename = path.basename(filename)
@@ -33,20 +37,57 @@ def copy_texture_files(serialized_materials, to_path):
                 makedirs(to_folder_path)
             copyfile(filename, to_file_path)
             shader["filename"] = path.join("texture", real_filename)
-    
+
+def serialize_object_w_materials(objects, materials):
+    objs = []
+    for obj in objects:
+        source_tags = obj.GetTags()
+        mats = []
+        for tag in source_tags:
+            if not isinstance(tag, c4d.TextureTag):
+                continue
+            c4d_mat = tag.GetMaterial()
+            mat = [mat for mat in materials if mat == c4d_mat][0]
+            mats.append(mat)
+        objs.append({"id": id(obj), "name": obj.GetName(), "materials": [mat.id for mat in mats]})
+    return objs
 
 def main():
+    # here are only active materials
     objects, materials = collect_data(doc)
     mats = serialize_materials(doc, materials)
+    objs = serialize_object_w_materials(objects, [Material(m, doc) for m in materials])
+    
     filepath = spawn_save_dialog()
+    if filepath is None:
+        print("No filepath given - aborting.")
+        return
+
     copy_texture_files(mats, path.dirname(filepath))
     with open(filepath, "w") as f:
-        f.write(json.dumps({"materials": mats}, indent=4))
+        f.write(json.dumps({"materials": mats, "objects": objs}, indent=4))
+    if c4d.documents.SaveDocument(doc, path.splitext(filepath)[0] + ".fbx", c4d.SAVEDOCUMENTFLAGS_EXPORTDIALOG, 1026370):
+        print("INFO: Exported FBX")
+    else:
+        print("ERR:  FBX not exported.")
+    
     print("Done.")
-    # spawn_save_dialog()
-    # with open("C:/Users/artcom/Desktop/C4D-Export/test1.json", "w") as f:
-    #     f.write(json.dumps({"materials": mats}, indent=4))
-    # print("Done.")
+    
+    
+    if False:
+        # here are ALL materials
+        materials = doc.GetMaterials()
+        cmaterials = doc.GetMaterials()
+        
+        conv_materials = [Material(mat, doc) for mat in materials]
+        conv_cmaterials = [Material(mat, doc) for mat in cmaterials]
+        
+        for m in conv_materials:
+            for cm in cmaterials:
+                print(m == cm)
+            print("-----------------------")
+            
+        mats = serialize_materials(doc, materials)
 
 if __name__ == '__main__':
     main()

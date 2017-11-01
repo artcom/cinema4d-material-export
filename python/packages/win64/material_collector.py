a18787fa8d3bdc60157706a4cb6b71bf18efe7ae
artcom <artcom@HOLODECK1>
2017-11-01 10:46:08 +0100

    should fix missing filenames in json and invalid copying

--- python/packages/win64/material_collector.py
+++ python/packages/win64/material_collector.py
@@ -1,129 +1,19 @@
-import c4d
-from material_strategies import collect_material_info
-import json
 from os import path
-
-
-MATERIAL_CHANNELS = {
-    "color": c4d.CHANNEL_COLOR,                  # +
-    "luminance": c4d.CHANNEL_LUMINANCE,          # +
-    "transparency": c4d.CHANNEL_TRANSPARENCY,    # +
-    "reflection": c4d.CHANNEL_REFLECTION,        # ~
-    "environment": c4d.CHANNEL_ENVIRONMENT,      # -
-    "fog": c4d.CHANNEL_FOG,                      # - 
-    "bump": c4d.CHANNEL_BUMP,                    # +
-    "alpha": c4d.CHANNEL_ALPHA,                  # +
-    "specular": c4d.CHANNEL_SPECULAR,            # +
-    "specular_color": c4d.CHANNEL_SPECULARCOLOR, # +
-    "glow": c4d.CHANNEL_GLOW,                    # ~
-    "displacement": c4d.CHANNEL_DISPLACEMENT,    # ~
-    "diffusion": c4d.CHANNEL_DIFFUSION,          # ~
-    "normal": c4d.CHANNEL_NORMAL,                # +
-    "any": c4d.CHANNEL_ANY                       # - 
-}
-
-SHADER_CHANNELS = {
-    "color": c4d.MATERIAL_COLOR_SHADER,
-    "luminance": c4d.MATERIAL_LUMINANCE_SHADER,
-    "transparency": c4d.MATERIAL_TRANSPARENCY_SHADER,
-    "reflection": c4d.MATERIAL_REFLECTION_SHADER,
-    "environment": c4d.MATERIAL_ENVIRONMENT_SHADER,
-    "bump": c4d.MATERIAL_BUMP_SHADER,
-    "alpha": c4d.MATERIAL_ALPHA_SHADER,
-    "specular": c4d.MATERIAL_SPECULAR_SHADER,
-    "displacement": c4d.MATERIAL_DISPLACEMENT_SHADER,
-    "diffusion": c4d.MATERIAL_DIFFUSION_SHADER,
-    "normal": c4d.MATERIAL_NORMAL_SHADER
-}
-
-SHADER_CONSTANTS = {
-    "blackpoint": c4d.BITMAPSHADER_BLACKPOINT,
-    "color_profile": c4d.BITMAPSHADER_COLORPROFILE,
-    "exposure": c4d.BITMAPSHADER_EXPOSURE,
-    "filename": c4d.BITMAPSHADER_FILENAME,
-    "gamma": c4d.BITMAPSHADER_GAMMA,
-    "interpolation": c4d.BITMAPSHADER_INTERPOLATION,
-    "white_point": c4d.BITMAPSHADER_WHITEPOINT
-}
-
-MATERIAL_NEEDED_CHANNELS = [
-    "color", "luminance", "transparency", "bump", "alpha",
-    "specular", "specular_color", "normal"
-]
-
-MATERIAL_OPTIONAL_CHANNELS = [
-    "reflection", "glow", "displacement", "diffusion"
-]
-
-class Material:
-    id, doc, name, c4d_material, tag_data = (None, None, None, None, None)
-
-    def __init__(self, c4d_material, doc):
-        if not isinstance(c4d_material, c4d.BaseMaterial):
-            raise ValueError("1st parameter is of invalid type: {}".format(type(c4d_material)))
-        self.c4d_material = c4d_material
-        self.name = c4d_material.GetName()
-        self.id = id(c4d_material)
-        self.tag_data = {}
-        self.doc = doc
-        for name, channel in MATERIAL_CHANNELS.iteritems():
-            channel_data, shader_data = self.probe_channel(c4d_material, channel, name)
-            if not channel_data is None:
-                self.tag_data[name] = channel_data
-                if not shader_data is None:
-                    self.tag_data[name]["shader"] = shader_data
-    
-    def probe_channel(self, mat, chan, chan_name):
-        if not chan_name in MATERIAL_NEEDED_CHANNELS:
-            return (None, None)
-        if not chan_name in SHADER_CHANNELS: # ignores specular_color - mby mistake
-            return (None, None)
-        has_state = mat.GetChannelState(chan)
-        if not has_state:
-            return (None, None)
-        channel_data = collect_material_info(mat, chan_name)
-        shader_data = self.collect_shader_data(channel_data["shader"])
-        # serialize vector - might need patching
-        for name, data_point in channel_data.iteritems():
-            if isinstance(data_point, c4d.Vector):
-                channel_data[name] = {'x': data_point[0], 'y': data_point[1], 'z': data_point[2]}
-        return (channel_data, shader_data)
-
-    def collect_shader_data(self, shader):
-        if shader is None:
-            return
-        data = {}
-        for name, constant in SHADER_CONSTANTS.iteritems():
-            value = shader[constant]
-            if isinstance(value, c4d.Vector):
-                value = {'x': value[0], 'y': value[1], 'z': value[2]}
-            data[name] = value
-            
-        tex_path = c4d.GenerateTexturePath(self.doc[c4d.DOCUMENT_PATH], data["filename"], "")
-        if tex_path is None:
-            tex_path = "INVALID PATH"
-            print("ERR:  Texture {} not found in project folder.".format(data["filename"]))
-            del data["filename"]
-        else:
-            data["filename"] = tex_path
-        return data
-
-    def to_json(self):
-        return {"id": self.id, "name": self.name, "tag_data": self.tag_data}
-
-    def __str__(self):
-        return "Material: name: [{}] - id: [{}]".format(self.name, self.id)
-
-    def __eq__(self, other):
-        if isinstance(other, Material):
-            return self.c4d_material.Compare(other.c4d_material)
-        if isinstance(other, c4d.Material):
-            return self.c4d_material.Compare(other)
-        else:
-            return False
+from materialexport.serializer_classes import Material
+import json
+import c4d
 
 
 def get_all_children(obj):
+    '''Recursive function searching all children AND sibling objects in a scene
+    (depth first search)
+
+    Args:
+        obj (c4d.BaseObject): The object to search from
+
+    Returns:
+        list: object itself, accompanied by all children AND siblings
+    '''
     all_seen = [obj]
     f_child = obj.GetDown()
     if not f_child:
@@ -136,6 +26,16 @@ def get_all_children(obj):
     return all_seen
 
 def collect_all_objects(objects):
+    '''Entry function to start a recursive object search for the objects in a
+    scene
+
+    Args:
+        objects (list): A list of objectes, ie. from doc.GetObjects()
+    
+    Returns:
+        list: Containing all objects, including input objects, found as
+              childrens and siblings
+    '''
     data = []
     for obj in objects:
         data.extend(get_all_children(obj))
@@ -143,29 +43,45 @@ def collect_all_objects(objects):
 
 
 def collect_data(doc):
-    document = c4d.documents.GetActiveDocument()
-    objects = collect_all_objects(document.GetObjects())    
+    ''' Collects all objects and materials inside the given document
+    Args:
+        doc (c4d.document): The document 
 
+    Returns:
+        tuple (list(c4d.BaseObject), list(c4d.TextureTag)): All found objects
+        and associated materials to those objects (unused materials are ignored)
+    '''
+    objects = collect_all_objects(doc.GetObjects())    
     mat_data = []
-    obj_data = []
     for obj in objects:
-        material_links = []
         source_tags = obj.GetTags()
         for tag in source_tags:
+            # filter all non-material tags - we still keep objects without
+            # materials, so any other software can look it up if it contains
+            # any material
             if not isinstance(tag, c4d.TextureTag):
                 continue
             mat = tag.GetMaterial()
             name = mat.GetName()
-            material_links.append({"name": name, "id": id(mat)})
             if not mat in mat_data:
                 mat_data.append(mat)
                 
     return objects, mat_data
 
 
-def serialize_materials(doc, materials):
+def serialize_materials(materials, doc):
+    '''Serializes all materials given to a json format.abs
+
+    Args:
+        materials (list(c4d.TextureTag)): All materials to serialize
+    
+    Returns:
+        list: JSON strings of materials
+    '''
     data = []
     unique_materials = []
+    # convert all c4d.TextureTag elements to serializer_classes.Material
+    # which then does the heavy lifting
     for material in materials:
         mat = Material(material, doc)
         if mat in data:
---
+++ python/packages/win64/serializer_classes.py
@@ -0,0 +1,124 @@
+import c4d
+from materialexport.constants import (
+    MATERIAL_CHANNELS, MATERIAL_NEEDED_CHANNELS, SHADER_CHANNELS, SHADER_CONSTANTS)
+from materialexport.material_strategies import collect_material_info
+
+
+class Material:
+    '''Material class is basically the same as 'c4d.TextureTag', but stores
+    a serializable string. Allows for comparison with other 'Material's and
+    'c4d.TextureTag's
+    '''
+    id, doc, name, c4d_material, tag_data = (None, None, None, None, None)
+
+    def __init__(self, c4d_material, doc):
+        '''Is currently a "serialize first, then never again" strategy, which
+        could be - in hindsight - be a mistake. Can be re-serialized with
+        obj.serialize()
+
+        Args:
+            c4d_material (c4d.TextureTag): Original material
+        '''
+        if not isinstance(c4d_material, c4d.BaseMaterial):
+            raise ValueError(
+                "1st parameter is of invalid type: {}".format(type(c4d_material)))
+        self.c4d_material = c4d_material
+        self.name = c4d_material.GetName()
+        self.id = id(c4d_material)
+        self.tag_data = {}
+        self.doc = doc
+        # serialize on object creating
+        self.serialize()
+
+    def serialize(self):
+        '''Serializes a c4d.TextureTag with all associated channels'''
+        for name, channel in MATERIAL_CHANNELS.iteritems():
+            # probe a channel
+            channel_data, shader_data = self.probe_channel(
+                self.c4d_material, channel, name)
+            # which could be empty and therefore irrelevant
+            if channel_data is None:
+                continue
+            self.tag_data[name] = channel_data
+            if not shader_data is None:
+                self.tag_data[name]["shader"] = shader_data
+
+    def probe_channel(self, mat, chan, chan_name):
+        '''Retrieves all channel and shader data
+        
+        Returns:
+            tuple(dict(channel), dict(shader)): channel and shader attributes
+        '''
+        # Filter out irrelevant channels
+        if not chan_name in MATERIAL_NEEDED_CHANNELS:
+            return (None, None)
+        # filter out shader-less channels (ie. global raytracer attributes)
+        if not chan_name in SHADER_CHANNELS:  # ignores specular_color - mby mistake
+            return (None, None)
+        # or if the channel is just empty
+        if not mat.GetChannelState(chan):
+            return (None, None)
+        # collect all attributes and shader data
+        channel_data = collect_material_info(mat, chan_name)
+        shader_data = self.collect_shader_data(channel_data["shader"])
+        # serialize vector - might need patching
+        for name, data_point in channel_data.iteritems():
+            if isinstance(data_point, c4d.Vector):
+                channel_data[name] = {'x': data_point[0],
+                                      'y': data_point[1], 'z': data_point[2]}
+        return (channel_data, shader_data)
+
+    def collect_shader_data(self, shader):
+        '''Retrieves all shader data for a given c4d.BaseShder. Needs associated
+        document to find a real path to associated textures (because obv. that's
+        a mess, too)
+        
+        Returns:
+            dict: containing all shader attributes
+        '''
+        if shader is None:
+            return
+        data = {}
+        for name, constant in SHADER_CONSTANTS.iteritems():
+            value = shader[constant]
+            if isinstance(value, c4d.Vector):
+                value = {'x': value[0], 'y': value[1], 'z': value[2]}
+            data[name] = value
+        # The tex-path can be: empty, not findable, missing or fine
+        # the filename is just a filename - no path or anything
+        tex_path = None
+        # when we don't have a document, we can't find the file anyway
+        if not self.doc is None:
+            tex_path = c4d.GenerateTexturePath(
+                self.doc[c4d.DOCUMENT_PATH], data["filename"], "")
+        # if it's None by not finding or not being able to search, then
+        # we remove the filename, because it has no data value
+        if tex_path is None:
+            tex_path = "INVALID PATH"
+            print("ERR:  Texture {} not found in project folder.".format(
+                data["filename"]))
+            del data["filename"]
+        data["filename"] = tex_path
+        return data
+
+    def to_json(self):
+        '''Outputs a friendly json-ready dictionary
+
+        Returns:
+            dict: Containing all material info ready to be serialized
+        '''
+        return {"id": self.id, "name": self.name, "tag_data": self.tag_data}
+
+    def __str__(self):
+        return "Material: name: [{}] - id: [{}]".format(self.name, self.id)
+
+    def __eq__(self, other):
+        if isinstance(other, Material):
+            return self.c4d_material.Compare(other.c4d_material)
+        if isinstance(other, c4d.Material):
+            return self.c4d_material.Compare(other)
+        else:
+            return False
+
+    def __hash__(self):
+        return hash([self.name, self.tag_data])
